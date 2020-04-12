535c00f2e80ee7a3855140afaf526bf1c85c7714
Zimtir <zimtir@mail.ru>
2020-04-13 01:22:42 +0300

    fix bold

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -8,46 +8,34 @@
   export let configuration
 
   export let classListModel
-
-  const isBold = () => {
-    let output = false
-
-    if (configuration.bold.enabled) {
-      if (item.index) {
-        output = item.index <= configuration.bold.count
-      } else {
-        output = true
-      }
-    }
-
-    return output
-  }
 </script>
 
-<Index
-  {classListModel}
-  visible={configuration.index.enabled}
-  index={item.index}
-/>
+{#if item}
+  <Index
+    {classListModel}
+    visible={configuration.index.enabled}
+    index={item.index}
+  />
 
-<Icon
-  {classListModel}
-  visible={configuration.icon.enabled}
-  src={item[configuration.icon.prop]}
-/>
+  <Icon
+    {classListModel}
+    visible={configuration.icon.enabled}
+    src={item[configuration.icon.prop]}
+  />
 
-<Description
-  {classListModel}
-  visible={configuration.description.enabled}
-  bold={isBold()}
-  descIsHTML={configuration.description.isHTML}
-  header={item[configuration.name.prop]}
-  body={item[configuration.description.prop]}
-/>
+  <Description
+    {classListModel}
+    visible={configuration.description.enabled}
+    bold={configuration.bold.enabled ? (item.index ? item.index <= configuration.bold.count : true) : false}
+    descIsHTML={configuration.description.isHTML}
+    header={item[configuration.name.prop]}
+    body={item[configuration.description.prop]}
+  />
 
-<Sub
-  {classListModel}
-  visible={configuration.point.enabled}
-  value={item[configuration.point.prop]}
-  needTimeago={configuration.point.isTimeago}
-/>
+  <Sub
+    {classListModel}
+    visible={configuration.point.enabled}
+    value={item[configuration.point.prop]}
+    needTimeago={configuration.point.isTimeago}
+  />
+{/if}
